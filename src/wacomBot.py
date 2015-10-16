be3da274cf5e18192f75de12d4be0e884a4f1882
s-chube <schubertr@etown.edu>
2015-10-16 15:08:26 -0400

    debugging wacomBot, recognizes OP keyword comment now

--- src/wacomBot.py
+++ src/wacomBot.py
@@ -12,7 +12,7 @@ from config_bot import *
 
 user_agent = ("wacomMod 0.1")
 r = praw.Reddit(user_agent=user_agent)
-r.login(REDDIT_USERNAME, REDDIT_PASS,disable_warning=True)
+r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning=True)
 
 if not os.path.isfile("posts_replied_to.txt"):
     posts_replied_to = []
@@ -22,30 +22,45 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = filter(None, posts_replied_to)
 
-subreddit = r.get_subreddit('stirusTEST')
+subreddit = r.get_subreddit('wacomTest')
 
-for submission in subreddit.get_hot(limit=5):
+for submission in subreddit.get_new(limit=5):
     if submission.id not in posts_replied_to:
         if re.search("Companion", submission.title, re.IGNORECASE):
-            if "do not delete" in submission.comments:
-                submission.add_comment("This post has been approved")
-            else:
-                submission.remove()
+            flat_comments = praw.helpers.flatten_tree(submission.comments)
+            for comment in flat_comments:
+                if comment.body == "do not delete":
+                    if comment.author == submission.author:
+                        submission.add_comment("This post has been approved")
+                        print "Approved a Companion-related post"
+                
+                else:
+                    submission.remove()
+                    print "Deleted a Companion-related post"
             
         if re.search("Surface", submission.title, re.IGNORECASE):
-            if "do not delete" in submission.comments:
-                submission.add_comment("This post has been approved")
-            else:
-                submission.remove()
+            flat_comments = praw.helpers.flatten_tree(submission.comments)
+            for comment in flat_comments:
+                if comment.body == "do not delete":
+                    if comment.author == submission.author:
+                        submission.add_comment("This post has been approved")
+                        print "Approved a Surface-related post"
+                
+                else:
+                    submission.remove()
+                    print "Deleted a Surface-related post"
         
         if re.search("Pro", submission.title, re.IGNORECASE):
-            if "do not delete" in submission.comments:
-                submission.add_comment("This post has been approved")
-            else:
-                submission.remove()
-
-print "Bot replying to : ", submission.title
-posts_replied_to.append(submission.id)
+            flat_comments = praw.helpers.flatten_tree(submission.comments)
+            for comment in flat_comments:
+                if comment.body == "do not delete":
+                    if comment.author == submission.author:
+                        submission.add_comment("This post has been approved")
+                        print "Approved a Pro-related post"
+                
+                else:
+                    submission.remove()
+                    print "Deleted a Pro-related post"
 
 with open("posts_replied_to.txt", "w") as f:
     for post_id in posts_replied_to:
