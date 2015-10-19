ac976b254a306ce3eb7b77bc61d1f1c1af17548f
s-chube <schubertr@etown.edu>
2015-10-18 23:37:38 -0400

    WacomBot 1.0 complete

--- src/wacomBot.py
+++ src/wacomBot.py
@@ -1,7 +1,7 @@
 '''
 Created on Oct 14, 2015
 
-@author: schubertr
+@author: /u/stirus
 '''
 
 import praw
@@ -10,11 +10,19 @@ import re
 import os
 from config_bot import *
 
-user_agent = ("wacomMod 0.1")
-r = praw.Reddit(user_agent=user_agent)
-r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning=True)
+def countComments(flat_comments): #function to count number of comments on a post
+    count = 0
+    for comment in flat_comments:
+        count += 1
+    return count
 
-if not os.path.isfile("posts_replied_to.txt"):
+user_agent = ("wacomMod 1.0")
+r = praw.Reddit(user_agent=user_agent)
+r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning=True) #logs in. I will update to new method when I figure out how
+
+safe_word = "do not delete" #put the phrase OP can comment to have the bot not delete the post here
+
+if not os.path.isfile("posts_replied_to.txt"): #creates or opens the file used to store posts replied to
     posts_replied_to = []
 else:
     with open("posts_replied_to.txt", "r") as f:
@@ -22,47 +30,86 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = filter(None, posts_replied_to)
 
-subreddit = r.get_subreddit('wacomTest')
+subreddit = r.get_subreddit('wacomTest') #put the subreddit name here
 
-for submission in subreddit.get_new(limit=5):
-    if submission.id not in posts_replied_to:
-        if re.search("Companion", submission.title, re.IGNORECASE):
-            flat_comments = praw.helpers.flatten_tree(submission.comments)
-            for comment in flat_comments:
-                if comment.body == "do not delete":
-                    if comment.author == submission.author:
-                        submission.add_comment("This post has been approved")
-                        print "Approved a Companion-related post"
+for submission in subreddit.get_new(limit=10): #gets the 10 newest submissions in the subreddit
+    if submission.id not in posts_replied_to: #if the current post is not one we checked already
+        if re.search("Companion", submission.title, re.IGNORECASE): #locates post with keyword
+            print("found companion post")
+            rem = 0
+            flat_comments = praw.helpers.flatten_tree(submission.comments) #gets comments
+            num = countComments(flat_comments) #counts comments
+            
+            if num == 0: #if no comments, remove post
+                submission.remove()
+                print "Deleted a Companion-related post"
+                posts_replied_to.append(submission.id)
                 
-                else:
+            else:
+                for comment in flat_comments:
+                    if comment.body == safe_word: #if OP commented the phrase, approve the post
+                        if comment.author == submission.author:
+                            submission.add_comment("This post has been approved")
+                            print "Approved a Companion-related post"
+                            posts_replied_to.append(submission.id)  
+                            rem = 1
+                
+                if rem != 1: #if not, delete it
                     submission.remove()
                     print "Deleted a Companion-related post"
+                    posts_replied_to.append(submission.id)  
             
-        if re.search("Surface", submission.title, re.IGNORECASE):
+        if re.search("Surface", submission.title, re.IGNORECASE): #follows same code as above, but with "Surface"
+            print("found surface post")
+            rem = 0
             flat_comments = praw.helpers.flatten_tree(submission.comments)
-            for comment in flat_comments:
-                if comment.body == "do not delete":
-                    if comment.author == submission.author:
-                        submission.add_comment("This post has been approved")
-                        print "Approved a Surface-related post"
+            num = countComments(flat_comments)
+            
+            if num == 0:
+                submission.remove()
+                print "Deleted a surface-related post"
+                posts_replied_to.append(submission.id)
                 
-                else:
+            else:
+                for comment in flat_comments:
+                    if comment.body == safe_word:
+                        if comment.author == submission.author:
+                            submission.add_comment("This post has been approved")
+                            print "Approved a Surface-related post"
+                            posts_replied_to.append(submission.id)
+                            rem = 1
+                
+                if rem != 1:
                     submission.remove()
                     print "Deleted a Surface-related post"
+                    posts_replied_to.append(submission.id)
         
-        if re.search("Pro", submission.title, re.IGNORECASE):
+        if re.search("Pro", submission.title, re.IGNORECASE): #follows same code as above, but with "Pro"
+            print("found a pro post")
+            rem = 0
             flat_comments = praw.helpers.flatten_tree(submission.comments)
-            for comment in flat_comments:
-                if comment.body == "do not delete":
-                    if comment.author == submission.author:
-                        submission.add_comment("This post has been approved")
-                        print "Approved a Pro-related post"
+            num = countComments(flat_comments)
+            
+            if num == 0:
+                submission.remove()
+                print "Deleted a Pro-related post"
+                posts_replied_to.append(submission.id)
                 
-                else:
+            else:
+                for comment in flat_comments:
+                    if comment.body == safe_word:
+                        if comment.author == submission.author:
+                            submission.add_comment("This post has been approved")
+                            print "Approved a Pro-related post"
+                            posts_replied_to.append(submission.id)
+                            rem = 1
+                
+                if rem != 1:
                     submission.remove()
                     print "Deleted a Pro-related post"
+                    posts_replied_to.append(submission.id)             
 
-with open("posts_replied_to.txt", "w") as f:
+with open("posts_replied_to.txt", "w") as f: #saves the posts we checked in the file so we don't check them again next time
     for post_id in posts_replied_to:
         f.write(post_id + "\n")
             
